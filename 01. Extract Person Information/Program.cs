fe390121519b8434909bf283178ca4456c8162ed
IvanHrIvanov <vinko96@abv.bg>
2020-11-23 21:40:51 +0200

    Add project files.

---
+++ 01. Extract Person Information/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> person = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                int indexOfNameStart = input.IndexOf('@');
+                int indexoFNameEnd = input.IndexOf('|');
+                string name = input.Substring(indexOfNameStart + 1, indexoFNameEnd - indexOfNameStart - 1);
+                int ageStartIndex = input.IndexOf('#');
+                int ageEndIndex = input.IndexOf('*');
+                string age = input.Substring(ageStartIndex + 1, ageEndIndex - ageStartIndex - 1);
+                Console.WriteLine($"{name} is {age} years old.");
+
+            }
+
+        }
+    }
+}
---
+++ 02. Ascii Sumator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstSymbol = Console.ReadLine();
+            string secondSymbol = Console.ReadLine();
+            string randomText = Console.ReadLine();
+                     
+            int startIndex = firstSymbol[0];
+            int endIndex = secondSymbol[0];
+
+            int sum = 0;
+
+            for (int i = 0; i < randomText.Length; i++)
+            {
+                int currentLeter = randomText[i];
+
+                if (currentLeter > startIndex && currentLeter < endIndex)
+                {
+                    sum += currentLeter;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
